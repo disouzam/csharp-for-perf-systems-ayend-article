2a4077fe08b9f97880c634d262253817584bd13a
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2024-08-26 18:35:14 -0300

    Added code from listing 1 on Codemag article that unzip file and read through it

--- CsharpCode/process_csv_file/Program.cs
+++ CsharpCode/process_csv_file/Program.cs
@@ -1,11 +1,20 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace process_csv_file;
 
 public static class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Processing a very large CSV file...");
+        Console.WriteLine("Reference: Ayende's article on https://www.codemag.com/article/2403091");
+
+        using var fs = new FileStream(path: "E:\\GitHubRepos\\csharp-for-perf-systems-ayende-article\\data.csv.gz", mode: FileMode.Open);
+        await foreach (var line in ZippedFileProcessing.GzipReadlAllLinesAsync(fs))
+        {
+            Console.WriteLine(line);
+        }
     }
 }
---
+++ CsharpCode/process_csv_file/ZippedFileProcessing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+
+namespace process_csv_file;
+
+internal static class ZippedFileProcessing
+{
+    /// <summary>
+    /// Code extracted from Listing 1 on https://www.codemag.com/article/2403091
+    /// </summary>
+    /// <param name="input"></param>
+    public static async IAsyncEnumerable<string> GzipReadlAllLinesAsync(Stream input)
+    {
+        using var gzipStream = new GZipStream(input, CompressionMode.Decompress);
+        using var reader = new StreamReader(gzipStream);
+
+        while (true)
+        {
+            string? line = await reader.ReadLineAsync();
+            if (line == null)
+                break;
+            yield return line;
+        }
+    }
+
+}
