7360aa241167ace532c339ba588183395926e8b9
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2024-08-26 18:04:11 -0300

    Adopted old-style of using program main method

--- CsharpCode/process_csv_file/Program.cs
+++ CsharpCode/process_csv_file/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace process_csv_file;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
