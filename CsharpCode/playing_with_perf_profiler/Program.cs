e286a22904c34caaada543fa5e853a1b6120a240
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2024-08-29 10:25:56 -0300

    Brand-new console app to play with Performance profiler in Visual Studio Professional 2022

---
+++ CsharpCode/playing_with_perf_profiler/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace playing_with_perf_profiler;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
