888ea11e3a00dda13e4bfebd5f0a30746e51edfc
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2024-08-29 10:45:48 -0300

    Added one variable creation to see .NET Object allocation diagnostics report

--- CsharpCode/playing_with_perf_profiler/Program.cs
+++ CsharpCode/playing_with_perf_profiler/Program.cs
@@ -6,6 +6,7 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var message = "Hello, World!";
+        Console.WriteLine(message);
     }
 }
